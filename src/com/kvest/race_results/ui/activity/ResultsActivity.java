f8904fe73fa89daa73339fcf94df9a93113dad4a
Roman Bielokon <roman_slavyansk@mail.ru>
2014-01-12 20:42:20 +0200

    added support Action Bar to application

--- src/com/kvest/race_results/ui/activity/ResultsActivity.java
+++ src/com/kvest/race_results/ui/activity/ResultsActivity.java
@@ -1,16 +1,10 @@
 package com.kvest.race_results.ui.activity;
 
-import android.content.ContentValues;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.widget.EditText;
+import android.support.v7.app.ActionBarActivity;
 import com.kvest.race_results.R;
-import com.kvest.race_results.contentprovider.ResultsProviderMetadata;
-import com.kvest.race_results.datastorage.table.ResultsTable;
-import com.kvest.race_results.utility.GroupHelper;
 
-public class ResultsActivity extends FragmentActivity {
+public class ResultsActivity extends ActionBarActivity {
     /**
      * Called when the activity is first created.
      */
