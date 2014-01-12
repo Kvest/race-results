5d01c07d94894e06146e97a3763e789e328bc9d6
Roman Bielokon <roman_slavyansk@mail.ru>
2014-01-12 13:48:57 +0200

    implemented GroupHelper

--- src/com/kvest/race_results/utility/GroupHelper.java
+++ src/com/kvest/race_results/utility/GroupHelper.java
@@ -7,5 +7,21 @@ package com.kvest.race_results.utility;
  * Time: 10:14
  * To change this template use File | Settings | File Templates.
  */
-public class GroupHelper {
+public abstract class GroupHelper {
+    public static final int FIRST_GROUP = 0;
+    public static final int SECOND_GROUP = 1;
+    public static final int THIRD_GROUP = 2;
+
+    public static final int FIRST_GROUP_MAX_AGE = 15;
+    public static final int SECOND_GROUP_MAX_AGE = 29;
+
+    public static int ageToGroup(int age) {
+        if (age <= FIRST_GROUP_MAX_AGE) {
+            return FIRST_GROUP;
+        } else if (age <= SECOND_GROUP_MAX_AGE) {
+            return SECOND_GROUP;
+        } else {
+            return THIRD_GROUP;
+        }
+    }
 }
